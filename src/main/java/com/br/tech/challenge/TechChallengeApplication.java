2ceab6d6bfdfb73b60f38dc1b2e8e737da5f2235
TadaoFS <Tads248624865@>
2025-06-09 21:06:51 -0300

    Initial commit

---
+++ src/main/java/com/br/tech/challenge/TechChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.br.tech.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/tech/challenge/TechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.tech.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
