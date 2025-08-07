77ffc7e2c819c09885938be44178b9e4b9fc0d76
Fernando <fn4586@gmail.com>
2025-08-07 19:43:08 -0300

    adicionando usuario na entidade enderecoadicionando usuario na entidade endereco

--- src/main/java/br/com/tech/challenge/services/EnderecoService.java
+++ src/main/java/br/com/tech/challenge/services/EnderecoService.java
@@ -1,25 +1,41 @@
 package br.com.tech.challenge.services;
 
-import java.util.List;
-import java.util.Optional;
-
 import br.com.tech.challenge.entities.Endereco;
+import br.com.tech.challenge.entities.Usuario;
 import br.com.tech.challenge.repositories.EnderecoRepository;
+import br.com.tech.challenge.repositories.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
-import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EnderecoService {
 
     private final EnderecoRepository enderecoRepository;
+    private final UsuarioRepository usuarioRepository;
 
-    public EnderecoService(EnderecoRepository enderecoRepository) {
+    public EnderecoService(EnderecoRepository enderecoRepository, UsuarioRepository usuarioRepository) {
         this.enderecoRepository = enderecoRepository;
+        this.usuarioRepository = usuarioRepository;
     }
 
     @Transactional
     public Endereco salvarEndereco(Endereco endereco) {
+        Long usuarioId = endereco.getUsuario() != null ? endereco.getUsuario().getId() : null;
+
+        if (usuarioId == null) {
+            throw new RuntimeException("Usuário é obrigatório para salvar um endereço.");
+        }
+
+        Usuario usuario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new RuntimeException("Usuário com ID " + usuarioId + " não encontrado."));
+
+        
+        endereco.setUsuario(usuario);
+        usuario.setEndereco(endereco);
+
         return enderecoRepository.save(endereco);
     }
 
@@ -35,18 +51,24 @@ public class EnderecoService {
 
     @Transactional
     public Endereco atualizarEndereco(Long id, Endereco enderecoAtualizado) {
-        Optional<Endereco> existenteOpt = enderecoRepository.findById(id);
+        Endereco existente = enderecoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Endereço com ID " + id + " não encontrado"));
 
-        if (existenteOpt.isEmpty()) {
-            throw new RuntimeException("Endereço com ID " + id + " não encontrado");
-        }
-
-        Endereco existente = existenteOpt.get();
         existente.setCep(enderecoAtualizado.getCep());
         existente.setLogradouro(enderecoAtualizado.getLogradouro());
         existente.setNumero(enderecoAtualizado.getNumero());
         existente.setBairro(enderecoAtualizado.getBairro());
         existente.setCidade(enderecoAtualizado.getCidade());
+        existente.setEstado(enderecoAtualizado.getEstado());
+
+        
+        if (enderecoAtualizado.getUsuario() != null && enderecoAtualizado.getUsuario().getId() != null) {
+            Usuario usuario = usuarioRepository.findById(enderecoAtualizado.getUsuario().getId())
+                    .orElseThrow(() -> new RuntimeException("Usuário com ID " + enderecoAtualizado.getUsuario().getId() + " não encontrado."));
+
+            existente.setUsuario(usuario);
+            usuario.setEndereco(existente);
+        }
 
         return enderecoRepository.save(existente);
     }
@@ -56,7 +78,7 @@ public class EnderecoService {
         if (!enderecoRepository.existsById(id)) {
             throw new RuntimeException("Endereço com ID " + id + " não encontrado");
         }
+
         enderecoRepository.deleteById(id);
     }
-
 }
